ec7a6d80e349d876ab792af65bc077a17a39d193
Amir Ashizhev <amirashizhev@yandex.ru>
2022-09-22 20:38:44 +0300

    feat: routes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,23 +1,18 @@
-import logo from '../../logo.svg';
 import './App.css';
+import { Route, Routes, Navigate } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path='/' element={<h1>О проекте</h1>}/>
+        <Route path='/movies' element={<h1>«Фильмы»</h1>}/>
+        <Route path='/saved-movies' element={<h1>«Сохранённые фильмы»</h1>}/>
+        <Route path='/profile' element={<h1>профилем пользователя</h1>}/>
+        <Route path='/signin' element={<h1>авторизации</h1>}/>
+        <Route path='/signup' element={<h1>регистрации</h1>}/>
+        <Route path='/*' element={<h1>не найдна</h1>}/>
+      </Routes>
     </div>
   );
 }
