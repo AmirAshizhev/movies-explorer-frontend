700ac03acd2b9927be677a4ab3b7f2d316a716a7
Amir Ashizhev <amirashizhev@yandex.ru>
2022-10-02 22:47:23 +0300

    feat: refactoring

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -15,12 +15,14 @@ const Movies = () => {
         <form className='movies__form'>
           <input className='movies__search-input' placeholder='Фильмы'></input>
           <button type='button' className='movies__search-btn'></button>
+          <span className='movies__search-span'></span>
         </form>
         <div className='movies__box'>
           <input className='movies__checkbox' type='checkbox'></input>
           <p className='movies__text'>Короткометражки</p>
         </div>
       </div>
+      <span className='movies__span'></span>
       <MoviesCardList/>
       <Footer/>
     </section>
